aecba1b00b79d92bc89d610d9ee3dfd68b90281a
zzzhr <zzzhr@hotmail.com>
2019-10-05 21:34:28 +0800

    bump grpc setting

--- server/server_manager.go
+++ server/server_manager.go
@@ -2,12 +2,12 @@ package server
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
 
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -23,15 +23,15 @@ func RunAndHold(s *grpc.Server, portNumber int) {
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		log.Print("Waiting SIGTERM...")
+		log.Infoln("Waiting SIGTERM...")
 		<-c
-		log.Print("Do clean jobs...")
+		log.Infoln("Do clean jobs...")
 		s.Stop()
 		// os.Exit(0)
 	}()
-	log.Printf("Starting server tcp on %v", portNumber)
+	log.Infof("Starting server tcp on %v", portNumber)
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", portNumber)
+		log.Errorf("failed to serve: %v", portNumber)
 		os.Exit(1)
 	}
 }
